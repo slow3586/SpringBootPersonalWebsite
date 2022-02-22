f96b800ca272f723266ca36bdfe95d6e2c9d3c0f
slow3586 <6337520+slow3586@users.noreply.github.com>
2022-02-22 12:17:35 +0700

    update: adding posts through postmapping works

--- src/main/java/demianskv/mywebsite/PostController.java
+++ src/main/java/demianskv/mywebsite/PostController.java
@@ -1,21 +1,52 @@
 package demianskv.mywebsite;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
-    private final PostRepository repository;
 
-    public PostController(PostRepository repository) {
-        this.repository = repository;
+    private final PostService service;
+
+    public PostController(PostService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path="/id/{id}")
+    public @ResponseBody
+    Optional<Post> findById(@PathVariable String id) {
+        Integer sId = Integer.parseInt(id);
+        return service.findById(sId);
     }
 
     @GetMapping(path="/all")
     public @ResponseBody Iterable<Post> findAll() {
-        return repository.findAll();
+        return service.findAll();
+    }
+
+    @GetMapping(path="/create_empty")
+    public ResponseEntity<Post> createEmpty() {
+        Post created = service.createPost();
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/id/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PostMapping(path="/add")
+    public ResponseEntity<Post> create(@RequestBody Post post) {
+        Post created = service.addPost(post);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/id/{id}")
+                .buildAndExpand(created.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(created);
     }
 }
---
+++ src/main/java/demianskv/mywebsite/PostService.java
@@ -0,0 +1,32 @@
+package demianskv.mywebsite;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public Post createPost(){
+        Post post = new Post();
+        return addPost(post);
+    }
+
+    public Post addPost(Post post){
+        repository.save(post);
+        return post;
+    }
+
+    public Optional<Post> findById(Integer id){
+        return repository.findById(id);
+    }
+
+    public Iterable<Post> findAll(){
+        return repository.findAll();
+    }
+}
